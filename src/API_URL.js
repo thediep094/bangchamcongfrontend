a628a001bb6c88e2e5f11902bb1323c61ced3c0e
thediep094 <thediep094@gmail.com>
2023-07-01 13:26:44 +0700

    update

--- src/API_URL.js
+++ src/API_URL.js
@@ -1,3 +1,3 @@
-export const API_URL = "http://localhost:3000";
+// export const API_URL = "http://localhost:3000";
 export const API_AI = "http://127.0.0.1:5000";
-// export const API_URL = "https://bangchamcongbackend.vercel.app";
+export const API_URL = "https://bangchamcongbackend.vercel.app";
