a76fd3e7e97eba4336b20e2baf61cca74950be3d
thediep094 <thediep094@gmail.com>
2023-07-04 11:24:17 +0700

    upadte

--- src/API_URL.js
+++ src/API_URL.js
@@ -1,3 +1,3 @@
 // export const API_URL = "http://localhost:3000";
-export const API_AI = "http://127.0.0.1:5000";
+export const API_AI = "https://convertvector-k3r7ofyh6q-ue.a.run.app";
 export const API_URL = "https://bangchamcongbackend.vercel.app";
